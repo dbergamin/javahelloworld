25bbdaa87a8f6da947662b08c466f621a47fcc46
Daniel Bergamin <daniel.bergamin@servicerocket.com>
2015-06-23 11:13:00 -0700

    Print the name of the current user

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main (String [] args) {
+        String username = System.getProperty("user.name");
         System.out.println("Hello World!");
+        System.out.println("Running as user: " + username);
     }
 }
